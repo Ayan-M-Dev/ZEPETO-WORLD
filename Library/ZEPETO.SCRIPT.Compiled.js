fad4b53f4f53ef07ba14c4f25cf0db56b3b36212
Ayan Masood <ayan.m.dev@gmail.com>
2022-12-17 08:48:06 +0530

    Change

---
+++ Library/ZEPETO.SCRIPT.Compiled.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const ZEPETO_Script_1 = require("ZEPETO.Script");
+class HelloWorld extends ZEPETO_Script_1.ZepetoScriptBehaviour {
+    Start() {
+        console.log('Hi, this Retr0! This log code is used to keep track of what your code is doing.');
+        console.warn('Hi, this is Retr0! This log code outputs a warning message, but your code is still able to run successfully.');
+        console.error('Hi, this is Retr0! This log code is displayed on the console when your application runs into an error.');
+    }
+}
+exports.default = HelloWorld;
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiSGVsbG9Xb3JsZC5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIkhlbGxvV29ybGQudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7QUFBQSxpREFBcUQ7QUFFckQsTUFBcUIsVUFBVyxTQUFRLHFDQUFxQjtJQUUzRCxLQUFLO1FBQ0gsT0FBTyxDQUFDLEdBQUcsQ0FBQyxpRkFBaUYsQ0FBQyxDQUFDO1FBQy9GLE9BQU8sQ0FBQyxJQUFJLENBQUMsOEdBQThHLENBQUMsQ0FBQztRQUM3SCxPQUFPLENBQUMsS0FBSyxDQUFDLHdHQUF3RyxDQUFDLENBQUM7SUFDMUgsQ0FBQztDQUNGO0FBUEQsNkJBT0MifQ==
