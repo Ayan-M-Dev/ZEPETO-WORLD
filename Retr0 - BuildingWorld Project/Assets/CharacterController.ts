f519b9a02681bea8b22e7c19c16db9128610e5d6
Ayan Masood <ayan.m.dev@gmail.com>
2022-12-18 13:56:44 +0530

    Update CharacterController.ts

--- Retr0 - BuildingWorld Project/Assets/CharacterController.ts
+++ Retr0 - BuildingWorld Project/Assets/CharacterController.ts
@@ -1,9 +1,15 @@
 import { ZepetoScriptBehaviour } from 'ZEPETO.Script'
+import { SpawnInfo, ZepetoPlayers, LocalPlayer, ZepetoCharacter } from 'ZEPETO.Character.Controller'
+
 
 export default class CharacterController extends ZepetoScriptBehaviour {
 
+
     Start() {    
-
+        ZepetoPlayers.instance.CreatePlayerWithZepetoId("", "retrovintage", new SpawnInfo(), true);
+ 
+        ZepetoPlayers.instance.OnAddedLocalPlayer.AddListener(() => {
+           let _player : LocalPlayer = ZepetoPlayers.instance.LocalPlayer;
+       });
     }
-
 }
